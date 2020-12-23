3fd46beee05dd02b413a98b09146925b6c8cd828
sai1 <koppuravurinagasai@gmail.com>
2020-12-23 22:45:24 +0530

    second commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import Planet from './components/Planet';
+
+
+function App() {
+  return (
+    <Router>
+      <Switch>
+        <Route path='/'>
+          <Planet />
+        </Route>
+      </Switch>
+      </Router>
+  );
+}
+
+export default App;
---
+++ src/components/Planet.js
@@ -0,0 +1,11 @@
+import React from 'react';
+class Planet extends React.Component {
+    render() {
+        return (
+            <div>
+                <p>Naasai</p>
+            </div>
+        )
+    }
+}
+export default Planet;
